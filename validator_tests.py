5cccfc057ea4dcb0b61209bd5c067819cbd94f3c
Matthew Norman <matthewn@surveymonkey.com>
2017-01-26 19:36:40 -0800

    Update the tests for functionality

--- validator_tests.py
+++ validator_tests.py
@@ -2,16 +2,24 @@ import unittest
 import sys
 
 from validate import validate_columns
+from validate import NoDataError
+from validate import ColumnMismatchError
+from validate import DuplicateColumnError
+
 
 class TestCSVValidator(unittest.TestCase):
 
-    def test_empty_arg(self):
-        with self.assertRaises(IOError):
-            validate_columns('')
-
     def test_row_lengths(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(NoDataError):
             validate_columns('test_files/tooFewColumns.csv')
+
+    def test_validate_columns_invalid_file(self):
+        r = validate_columns('/this/better/not/exist/on/your/system')
+        assert r is None
+
+    def test_validate_columns_duplicate_columns(self):
+        with self.assertRaises(DuplicateColumnError):
+            validate_columns('test_files/duplicate_columns.csv')
 
 
 if __name__ == '__main__':
